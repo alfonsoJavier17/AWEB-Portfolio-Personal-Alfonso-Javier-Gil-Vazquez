836e794caf82573bb83a06bf47b6b8c7274da5df
AlfonsoJavier17 <gilvazquezalfonsojavier@gmail.com>
2025-05-06 14:28:45 +0200

    terminar script del modo nocturno

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
 //pongo los colores
 
-const color_header = ["rgb(59, 57, 57)","rgb(233, 228, 228)"];
-const color_main = ["rgb(22, 20, 20)","rgb(219, 212, 212)"];
+const color_header = ["rgb(233, 228, 228)","rgb(59, 57, 57)"];
+const color_main = ["rgb(219, 212, 212)","rgb(22, 20, 20)"];
 
 //variables
 
 const boton = document.querySelector(".body__header__figure__btn");
 const header = document.querySelector (".body__header");
 const main = document.querySelector (".body__main");
+const body = document.querySelector (".body");
 
 //funcion del boton
 
-boton.addEventListener("click", function(){ 
-    main.style.backgroundColor = color_main[2];
-    header.style.backgroundColor = color_header[2];
-})
+let i=0;
+
+boton.addEventListener("click", function(){    
+    if (i == 0) {
+        header.style.backgroundColor = color_header[1];
+        main.style.backgroundColor = color_main[1];
+        body.style.color = "white";
+        i = 1;
+    } else {
+        header.style.backgroundColor = color_header[0];
+        main.style.backgroundColor = color_main[0];
+        body.style.color = "black";
+        i = 0; 
+    }
+});
